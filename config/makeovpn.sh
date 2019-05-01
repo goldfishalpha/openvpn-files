c0d3218d70d3ebedfbf2823d892c0936bcde5bcf
goldfishalpha <41950061+goldfishalpha@users.noreply.github.com>
2019-05-01 10:35:43 +0100

    First upload
    Simple client, server and makeovpn files

---
+++ config/makeovpn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# First argument: Client identifier
+
+KEY_DIR=./keys
+OUTPUT_DIR=./client
+BASE_CONFIG=./config/client.conf
+
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${KEY_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${KEY_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-auth>') \
+    ${KEY_DIR}/ta.key \
+    <(echo -e '</tls-auth>') \
+    > ${OUTPUT_DIR}/${1}.ovpn
